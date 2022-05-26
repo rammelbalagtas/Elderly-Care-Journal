374be633ee5ea134d7ddc641f9a16470d2671ae0
rammelbalagtas <rammel.balagtas@gmail.com>
2022-05-25 20:01:26 -0400

    Build the Create New Shift Page

---
+++ ElderlyCareJournal/ViewController/ShiftListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ShiftListViewController.swift
+//  ElderlyCareJournal
+//
+//  Created by Rammel on 2022-05-25.
+//
+
+import UIKit
+
+class ShiftListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
